57f8c19b7f908307e1302530aaf248ba9ef00e82
zthxxx <zth_9451@qq.com>
2017-02-05 15:30:47 +0800

    Support: task lists in markdown but render by js in front end

--- source/js/main.js
+++ source/js/main.js
@@ -58,5 +58,37 @@
             $('body, html').animate({ scrollTop: 0 }, 600);
         });
     }
-
+    
+    // Task lists in markdown
+    $('ul > li').each(function() {
+        var taskList = {
+            field: this.textContent.substring(0, 2),
+            check: function(str) {
+                var re = new RegExp(str);
+                return this.field.match(re);
+            }
+        }
+        var string = ["[ ]", ["[x]", "checked"]];
+        var checked = taskList.check(string[1][0]);
+        var unchecked = taskList.check(string[0]);
+        var $current = $(this);
+        function update(str, check) {
+            var click = ["disabled", ""];
+            $current.html($current.html().replace(
+              str, "<input type='checkbox' " + check + " " + click[1] + " >")
+            )
+        }
+        if (checked || unchecked) {
+            this.classList.add("task-list");
+            if (checked) {
+                update(string[1][0], string[1][1]);
+                this.classList.add("check");
+            } else {
+                update(string[0], "");
+            }
+        }
+    })
+    $(document).on('click', 'input[type="checkbox"]', function (event) {
+        event.preventDefault();
+    });
 })(jQuery);
